f627a31bee0135be1741adf35a466e28a46cbf61
JYPark09 <jyp10987@gmail.com>
2019-11-04 21:26:18 +0900

    Fix: Add namespace

--- UnitTest/Cards/CardTests.cc
+++ UnitTest/Cards/CardTests.cc
@@ -1,10 +1,15 @@
 #include "gtest/gtest.h"
 
-#include <Poker/Cards/card.h>
+#include <Poker/Cards/Card.h>
 
-TEST(CardTests, TEST01) {
-    Card c1(clover, seven), c2(heart, six), c3(clover, six);
-    
+using namespace Poker;
+
+TEST(CardTests, TEST01)
+{
+    Card c1(CardShape::CLOVER, CardNumber::SEVEN),
+        c2(CardShape::HEART, CardNumber::SIX),
+        c3(CardShape::CLOVER, CardNumber::SIX);
+
     EXPECT_EQ(c1 > c2, 1);
     EXPECT_EQ(c1 > c3, 1);
     EXPECT_EQ(c2 > c3, 1);
