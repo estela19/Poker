464e9b026269b9f70ef042f8d8207883eaeaee91
JYPark09 <jyp10987@gmail.com>
2019-11-04 21:24:51 +0900

    Fix: Separate enumerator from Card

---
+++ Includes/Poker/Cards/CardEnums.h
@@ -0,0 +1,29 @@
+// Copyright(C) 2019 Sooyeon Kim, Gyeonguk Chae, Junyeong Park
+
+namespace Poker
+{
+enum class CardShape
+{
+    CLOVER,
+    HEART,
+    DIAMOND,
+    SPADE
+};
+
+enum class CardNumber
+{
+    TWO,
+    THREEE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
+}
