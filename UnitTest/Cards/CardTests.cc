992e34d38afe13ddc77aa374d5513dad0a1e0478
JYPark09 <jyp10987@gmail.com>
2019-11-05 10:02:11 +0900

    Test: Add test case for Card Compare

--- UnitTest/Cards/CardTests.cc
+++ UnitTest/Cards/CardTests.cc
@@ -4,13 +4,18 @@
 
 using namespace Poker;
 
-TEST(CardTests, TEST01)
+TEST(CardTests, Compare)
 {
     Card c1(CardShape::CLOVER, CardNumber::SEVEN),
         c2(CardShape::HEART, CardNumber::SIX),
         c3(CardShape::CLOVER, CardNumber::SIX);
 
-    EXPECT_EQ(c1 > c2, 1);
-    EXPECT_EQ(c1 > c3, 1);
-    EXPECT_EQ(c2 > c3, 1);
+    EXPECT_TRUE(c1 > c2);
+    EXPECT_TRUE(c1 > c3);
+    EXPECT_TRUE(c2 > c3);
+
+    Card c4(CardShape::CLOVER, CardNumber::ACE),
+        c5(CardShape::HEART, CardNumber::SIX);
+
+    EXPECT_FALSE(c5 > c4);
 }
