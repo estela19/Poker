c7c51cf0c8ae7e850758d991d7385e46d28bf553
JYPark09 <jyp10987@gmail.com>
2019-11-11 22:34:34 +0900

    Refactor: Move DeckType and Fix build errors
    - Move DeckType from CardDecks.h to CardDeck.h
    - Fix build errors with hash<CardDeck>

--- Includes/Poker/Cards/CardDeck.h
+++ Includes/Poker/Cards/CardDeck.h
@@ -9,6 +9,23 @@
 
 namespace Poker
 {
+enum class DeckType
+{
+    ROYAL_STRAIGHT_FLUSH,
+    BACK_STRAIGHT_FLUSH,
+    STRAIGHT_FLUSH,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    FLUSH,
+    MOUNTAIN,
+    BACK_STRAIGHT,
+    STRAIGHT,
+    THREE_OF_A_KIND,
+    TWO_PAIR,
+    ONE_PAIR,
+    TOP
+};
+
 class CardDeck final
 {
  public:
@@ -30,9 +47,9 @@ struct hash<Poker::CardDeck>
 {
     long long operator()(const Poker::CardDeck& t) const
     {
-        long long cardList = (1 << 52) - 1;
+        [[maybe_unused]] long long cardList = (1ll << 52) - 1;
 
-        return;
+        return 0;
     }
 };
 }  // namespace std
