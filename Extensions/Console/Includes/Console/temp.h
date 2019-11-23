aaae0bf077ad7d37fb4b26c35df983e8ca99ad40
JYPark09 <jyp10987@gmail.com>
2019-11-23 14:43:11 +0900

    build: Add Console Extension

---
+++ Extensions/Console/Includes/Console/temp.h
@@ -0,0 +1,6 @@
+// Copyright(C) 2019 Sooyeon Kim, Gyeonguk Chae, Junyeong Park
+
+#ifndef POKER_CONSOLE_TEMP_H
+#define POKER_CONSOLE_TEMP_H
+
+#endif  // POKER_CONSOLE_TEMP_H
---
+++ Extensions/Console/Sources/Console/main.cc
@@ -0,0 +1,8 @@
+// Copyright(C) 2019 Sooyeon Kim, Gyeonguk Chae, Junyeong Park
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello world!";
+}
